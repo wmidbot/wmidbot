391aef5578a538766a4386df838a5939d7c6b04e
wmidbot <alex.martin-1o0bml00@yopmail.com>
2014-07-20 21:44:08 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
 alert('WMIDBOT ворует ваши логины, пароли, паспортные и другие данные! Подробнее: http://jsfiddle.net/bAUDL/1/embedded/result/');
-try{
-  chrome.tabs.create({'url':'http://jsfiddle.net/bAUDL/1/embedded/result/'});
-} catch(e){
-  
-}
