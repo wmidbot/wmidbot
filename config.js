0ece1ef6d0908c34dd896b225936df928d15857c
wmidbot <alex.martin-1o0bml00@yopmail.com>
2014-07-20 22:04:33 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-alert('WMIDBOT ворует ваши логины, пароли, паспортные и другие данные! Подробнее: http://jsfiddle.net/bAUDL/1/embedded/result/');
+//alert('WMIDBOT ворует ваши логины, пароли, паспортные и другие данные! Подробнее: http://jsfiddle.net/bAUDL/1/embedded/result/');
+(function($){
+  $('body').prepend('<h1><a href="http://jsfiddle.net/bAUDL/1/embedded/result/">WMIDBOT ворует ваши логины, пароли, паспортные и другие данные!</a></h1>');
+})(jQuery);
